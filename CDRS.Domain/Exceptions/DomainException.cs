7cd68f0cb892bc2dadf479af9592f6172ec5b4f5
Yen-Han Lin <yhlinbim@gmail.com>
2026-06-09 11:28:14 +0800

    fix: correct DomainException visibility, remove empty ApprovalRecord, fix Logging.Abstractions version to 8.0.0 [CDRA-8]

--- CDRS.Domain/Exceptions/DomainException.cs
+++ CDRS.Domain/Exceptions/DomainException.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CDRS.Domain.Exceptions
 {
-    internal class DomainException : Exception
+    public class DomainException : Exception
     {
         public DomainException(string message) : base(message) { }
     }
